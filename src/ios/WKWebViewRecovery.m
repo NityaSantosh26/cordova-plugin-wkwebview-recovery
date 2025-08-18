2304f09c8b6370b4ff8163a5807df3de365975ca
Nitya Santosh <130960789+NityaSantosh26@users.noreply.github.com>
2025-08-18 20:19:01 +0530

    code refactoring WKWebViewRecovery.m
    removed the duplicate @interface block.

--- src/ios/WKWebViewRecovery.m
+++ src/ios/WKWebViewRecovery.m
@@ -1,10 +1,4 @@
-#import <Cordova/CDV.h>
-#import <WebKit/WebKit.h>
-
-@interface WKWebViewRecovery : CDVPlugin <WKNavigationDelegate>
-@property (nonatomic, strong) WKWebView *wkWebView;
-@property (nonatomic, weak) id<WKNavigationDelegate> previousNavigationDelegate;
-@end
+#import "WKWebViewRecovery.h"
 
 @implementation WKWebViewRecovery
 
