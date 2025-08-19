86584a63bf950a848cb5de9fc1560cd62781d8ae
Nitya Santosh <nityasantosh.channoju@planonsoftware.com>
2025-08-19 10:16:14 +0530

    Update WKWebViewRecovery.js

--- www/WKWebViewRecovery.js
+++ www/WKWebViewRecovery.js
@@ -1,4 +1,1 @@
-cordova.define("cordova-plugin-wkwebview-recovery.WKWebViewRecovery", function(require, exports, module) {
-	// Native plugin works without JS calls; export a minimal API under cordova.plugins.WKWebViewRecovery
-	module.exports = {};
-});
+module.exports = {};
